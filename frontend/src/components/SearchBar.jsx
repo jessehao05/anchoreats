a43e9017aa8cde9f8f3abea44bf7fc01f1c2b379
Jesse Hao <jehyu77@gmail.com>
2025-09-03 22:13:34 -0500

    search icon, html title and icon

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Search } from "lucide-react";
 
 
 const SearchBar = ({ onSearch }) => {
@@ -12,19 +13,21 @@ const SearchBar = ({ onSearch }) => {
 
   return (
     <div className="mb-2">
-        <label className="input input-bordered flex items-center gap-2">
-            <input type="text" className="grow" placeholder="Search..." onChange={handleChange}/>
+        <label className="input input-bordered flex items-center">
             <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4 opacity-70">
-                <path
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70 flex-shrink-0">
+              <path
                 fillRule="evenodd"
                 d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
                 clipRule="evenodd" />
             </svg>
+            <input type="text" className="grow ml-2" placeholder="Search..." onChange={handleChange}/>
         </label>
+        
+        
     </div>
   )
 }
