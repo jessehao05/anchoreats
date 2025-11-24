50131ecaf4d6dd1294c2e5b8c7d259e66bff0988
Jesse Hao <jehyu77@gmail.com>
2025-11-24 15:00:51 -0500

    vercel/render setup for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 })
 app.use(
     cors({
-    origin: "http://localhost:5173"
+    origin: process.env.FRONTEND_URL || "http://localhost:5173"
 }));
 
 app.use("/api", allRoutes);
