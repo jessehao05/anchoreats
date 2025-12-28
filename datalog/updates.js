d95bd3a929e5601e86b5e2053d3abab0371351e5
Jesse Hao <jehyu77@gmail.com>
2025-12-28 14:48:07 -0500

    added folder for any changes in data

---
+++ datalog/updates.js
@@ -0,0 +1,89 @@
+
+// Update 1 - 12/27/25
+export const updateOne = [
+    { 
+        name: "Dumpling House", 
+        updates: 
+        { 
+            description: "Chinese restaurant",
+            website: "https://dumplinghousenashville.net/",
+            location: { lat: 36.136742, lng: -86.802379 },
+            hours: {
+                mon: [{ open: "11:30", close: "21:30" }],
+                tue: [{ open: "11:30", close: "21:30" }],
+                wed: [{ open: "11:30", close: "21:30" }],
+                thu: [{ open: "11:30", close: "21:30" }],
+                fri: [{ open: "11:30", close: "22:30" }],
+                sat: [{ open: "11:30", close: "22:30" }],
+                sun: [{ open: "11:30", close: "21:30" }]
+            }
+        } 
+    },
+    { 
+        name: "Hattie B's", 
+        updates: 
+        { 
+            description: "Fried chicken, Southern sides",
+            website: "https://www.hattieb.com/",
+            location: { lat: 36.151307, lng: -86.796650 },
+            hours: {
+                mon: [{ open: "10:45", close: "22:00" }],
+                tue: [{ open: "10:45", close: "22:00" }],
+                wed: [{ open: "10:45", close: "22:00" }],
+                thu: [{ open: "10:45", close: "22:00" }],
+                fri: [{ open: "10:45", close: "23:59" }],
+                sat: [{ open: "10:45", close: "23:59" }],
+                sun: [{ open: "10:45", close: "22:00" }]
+            }
+        } 
+    },
+    { 
+        name: "Torchy's Tacos", 
+        updates: 
+        { 
+            description: "Taco restaurant",
+            website: "https://torchystacos.com/",
+            location: { lat: 36.149041, lng: -86.798495 },
+            hours: {
+                mon: [{ open: "11:00", close: "23:00" }],
+                tue: [{ open: "11:00", close: "23:00" }],
+                wed: [{ open: "11:00", close: "23:00" }],
+                thu: [{ open: "11:00", close: "23:59" }],
+                fri: [{ open: "11:00", close: "01:00" }],
+                sat: [{ open: "11:00", close: "01:00" }],
+                sun: [{ open: "11:00", close: "22:00" }]
+            }
+        } 
+    },
+    { 
+        name: "Urban Cookhouse", 
+        updates: 
+        { 
+            description: "Sandwiches, wraps, salads",
+            website: "https://www.urbancookhouse.com/",
+            location: { lat: 36.150518, lng: -86.796896 },
+            hours: {
+                mon: [{ open: "10:30", close: "16:00" }],
+                tue: [{ open: "10:30", close: "16:00" }],
+                wed: [{ open: "10:30", close: "16:00" }],
+                thu: [{ open: "10:30", close: "16:00" }],
+                fri: [{ open: "10:30", close: "15:00" }],
+                sat: [{ open: "10:30", close: "15:00" }],
+                sun: [{ open: "10:30", close: "15:00" }]
+            }
+        } 
+    },
+    { 
+        name: "Woodlands", 
+        updates: 
+        { 
+            name: "Woodlands Indian Cuisine",
+            description: "Indian vegetarian restaurant",
+        } 
+    },    
+];
+
+export const deleteOne = [
+    "Bombay Palace",
+    "Jet's Pizza"
+]
